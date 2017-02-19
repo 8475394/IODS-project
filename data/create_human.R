502ee12a3fd3b8189bdade6bd58d899a1dd9bf3a
8475394 <heidi.maanonen@hotmail.com>
2017-02-19 14:43:33 +0200

    Data Wrangling with human data

--- data/create_human.R
+++ data/create_human.R
@@ -1,6 +1,13 @@
 # Author: Heidi Maanonen
-# Date: 17.2.2017
-# File description: Data Wrangling for next week's exercise
+
+# Original Date: 17.2.2017
+# Continuing the Human data wrangling, Date: 19.2.2017
+
+# File description 1: Initial Data Wrangling for next week's exercise (Dimensionality reduction techniques)
+# File description 2: Data Wrangling for Dimensionality reduction techniques
+
+# Data Reference: "http://hdr.undp.org/en/content/human-development-index-hdi"
+
 
 # Reading the "Human development" data
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
@@ -29,7 +36,7 @@ table(is.na(gii)) # 132 missing values
 colnames(hd) # Printing just the column names 
 
 library(dplyr)
-hd <- rename(hd, HDI_rank = HDI.Rank,
+hdnew <- rename(hd, HDI_rank = HDI.Rank,
        country = Country,
        HDI_index = Human.Development.Index..HDI.,
        Elife = Life.Expectancy.at.Birth, 
@@ -38,7 +45,7 @@ hd <- rename(hd, HDI_rank = HDI.Rank,
        GNIpc = Gross.National.Income..GNI..per.Capita,
        GH_rank = GNI.per.Capita.Rank.Minus.HDI.Rank)
 
-colnames(hd) # Verifying the column names
+colnames(hdnew) # Verifying the column names
 
 # HDI_rank = The rank of a country in the Human development index
 # country = Country
@@ -55,7 +62,7 @@ colnames(hd) # Verifying the column names
 
 colnames(gii) # Printing just the column names
 
-gii <- rename(gii,
+giinew <- rename(gii,
               GII_rank = GII.Rank,
               country = Country,
               GII_index = Gender.Inequality.Index..GII.,
@@ -67,7 +74,7 @@ gii <- rename(gii,
               Flabour = Labour.Force.Participation.Rate..Female.,
               Mlabour = Labour.Force.Participation.Rate..Male.)
 
-colnames(gii) # Verifying the column names
+colnames(giinew) # Verifying the column names
 
 # GII_rank = The rank of a country in the Gender inequality index
 # country = Country
@@ -83,20 +90,77 @@ colnames(gii) # Verifying the column names
 
 # Creating 2 new variables
 
-gii <- mutate(gii, SFM_edu = SFedu / SMedu, FMlabour = Flabour / Mlabour)
-colnames(gii)
+giinew <- mutate(giinew, SFM_edu = SFedu / SMedu, FMlabour = Flabour / Mlabour)
+colnames(giinew)
 
 
 # Joining the datasets
 
 join_by <- c("country")
-human <- inner_join(hd, gii, by = c(join_by), suffix = c(".hd", ".gii"))
+human <- inner_join(hdnew, giinew, by = c(join_by), suffix = c(".hdnew", ".giinew"))
 
 colnames(human)
 glimpse(human) # 195 observations and 19 variables
+str(human)
 
 
 # Saving the create_human and joined "human" datset to the data folder
 
 setwd("C:/Users/heidi/Documents/YLIOPISTO/TILASTOTIEDE/INTRODUCTION TO OPEN DATA SCIENCE/IODS-project/data")
 getwd()
+
+
+# Continuing the Wrangling
+
+library(tidyr) 
+library(stringr)
+library(dplyr)
+
+# Mutating the GNI variable to numeric
+str(human$GNIpc)
+human$GNIpc <- str_replace(human$GNIpc, pattern= ",", replace="") %>% as.numeric(human$GNIpc)
+str(human$GNIpc) # verifying the numeric content
+
+# Excluding unneeded variables, keeping the columns country (Country),SFM_edu (Edu2.FM), FMlabour (Labo.FM), 
+# Eedu (Edu.Exp), Elife (Life.Exp), GNIpc (GNI), mort_ratio (Mat.Mor), ad_birth (Ado.Birth), seats (Parli.F)
+
+keep <- c("country", "SFM_edu", "FMlabour", "Eedu", "Elife", "GNIpc", "mort_ratio", "ad_birth", "seats")
+human <- select(human, one_of(keep))
+str(human) # Verifying the content: 195 observation and 9 variables
+
+
+# Removing rows with missing values
+
+complete.cases(human)
+comp <- complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human)) # TRUE are the complete cases without NA:s
+str(human) 
+
+# Removing the rows with missing values 
+
+human_ <- filter(human, comp == TRUE)
+str(human_) # 162 observations and 9 variables
+
+
+# Removing the observations which relate to regions instead of countries
+
+tail(human_, 20) # last 7 variables are regions
+last <- nrow(human_) - 7
+human_ <- human_[1:last,]
+tail(human_, 10) # veryfying the tail
+
+# Defining the row names of the data by the country names 
+rownames(human_) <- human_$country
+str(human_) # 155 observations and 9 variables 
+colnames(human_)
+
+# Removing the country name column from the data
+human_ <- dplyr::select(human_, -country)
+
+# Saving the Dataset
+write.csv(human_, file = "human_1.csv", row.names = TRUE)
+
+# Checking that data reading works and r can actually use the file
+human_1 <- read.csv("human_1.csv")
+str(human_1)
+
